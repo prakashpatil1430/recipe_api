fff922886956ce846ef59841e60d5538bd6b5124
prakashpatil1430 <91424925+prakashpatil1430@users.noreply.github.com>
2023-08-19 13:57:18 +0530

    adding User customaztion and test classes

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,42 @@
-from django.db import models  # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new User"""
+        if not email:
+            raise ValueError("User must have email Id.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creating a superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the System"""
+
+    email = models.EmailField(max_length=250, unique=True)
+    name = models.CharField(max_length=60)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
